0686186c19b9e864f65c8bcbdd87d549ca6030ce
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-23 15:49:18 +0100

    Add simple ping command

---
+++ src/Xylox.Discord/Commands/Modules/Ping.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Xylox.Discord.Commands.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("Ping")]
+        public async Task PingCommand()
+        {
+            await ReplyAsync("Pong!");
+        }
+    }
+}
