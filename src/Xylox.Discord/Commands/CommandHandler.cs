73ae5165c71e938deb2b6971f9eca7b24781cded
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-23 14:41:41 +0100

    Start of command service

---
+++ src/Xylox.Discord/Commands/CommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Commands;
+
+namespace Xylox.Discord.Commands
+{
+    public class CommandHandler
+    {
+        private readonly XyloxDiscord _bot;
+        private readonly CommandService _commandService;
+        public CommandHandler(XyloxDiscord bot)
+        {
+            _bot = bot;
+            _commandService = _bot.CommandService;
+        }
+    }
+}
