8cd98a1c253d09c600b55963154f61e8e5de1d57
Joel Parkinson <joelparkinson@live.co.uk>
2019-11-02 22:25:30 +0000

    Setup for Music service

---
+++ src/Xylox.Services/Entities/XyloxTrack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xylox.Services.Entities
+{
+    public class XyloxTrack
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Url { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ src/Xylox.Services/IMusicService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xylox.Services.Entities;
+
+namespace Xylox.Services
+{
+    public interface IMusicService
+    {
+        Task<IEnumerable<XyloxTrack>> GetPlaylistAsync(ulong id);
+        Task<XyloxTrack> PlayTrackAsync(string query, ulong id);
+        Task<XyloxTrack> PlayTrackAsync(Uri uri, ulong id);
+        Task<XyloxTrack> SkipTrackAsync(int trackId, ulong id);
+        Task<XyloxTrack> SkipTrackAsync(ulong id);
+        Task<string> ConnectAsync(ulong voiceChannel, ulong textChannel);
+        Task<string> PauseAsync(ulong id);
+        Task<string> ResumeAsync(ulong id);
+        Task<string> StopAsync(ulong id);
+        Task<string> SetVolumeAsync(int level, ulong id);
+    }
+}
