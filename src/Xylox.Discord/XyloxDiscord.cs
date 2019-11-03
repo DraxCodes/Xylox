95d863e89924efd36b7d61920bc57d7759e78eae
Joel Parkinson <joelparkinson@live.co.uk>
2019-11-03 20:23:16 +0000

    Add initialise for MusicService
    Service should initialize when the Discord client invokes the ready event.

--- src/Xylox.Discord/XyloxDiscord.cs
+++ src/Xylox.Discord/XyloxDiscord.cs
@@ -3,6 +3,7 @@ using Discord.WebSocket;
 using System.Threading.Tasks;
 using Xylox.Discord.Commands;
 using Xylox.Discord.Config;
+using Xylox.Services;
 
 namespace Xylox.Discord
 {
@@ -13,13 +14,17 @@ namespace Xylox.Discord
         private readonly Logger _logger;
         private readonly IXyloxConfig _xyloxConfig;
         private readonly XyConf _xyConf;
+        private readonly IMusicService _musicService;
 
-        public XyloxDiscord(DiscordSocketClient discordClient, CommandHandler commandHandler, Logger logger, IXyloxConfig xyloxConfig)
+        public XyloxDiscord(DiscordSocketClient discordClient, CommandHandler commandHandler,
+            Logger logger, IXyloxConfig xyloxConfig, IMusicService musicService)
         {
             _discordClient = discordClient;
             _commandHandler = commandHandler;
             _logger = logger;
             _xyloxConfig = xyloxConfig;
+            _musicService = musicService;
+
             _xyConf = _xyloxConfig.GetConfig();
         }
 
@@ -45,6 +50,7 @@ namespace Xylox.Discord
         {
             await _discordClient.SetStatusAsync(UserStatus.Idle);
             await _discordClient.SetGameAsync(name: _xyConf.Status, type: ActivityType.Listening);
+            await _musicService.InitializeWhenReadyAsync();
         }
     }
 }
