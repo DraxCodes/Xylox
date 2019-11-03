d1f5f8e9d6fae729091783f217930f7241e99d56
Joel Parkinson <joelparkinson@live.co.uk>
2019-11-03 20:12:02 +0000

    Implement IMusicService

--- src/Xylox.Audio/MusicService.cs
+++ src/Xylox.Audio/MusicService.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Victoria;
+using Victoria.Enums;
+using Victoria.EventArgs;
 using Xylox.Services;
 using Xylox.Services.Entities;
 
@@ -8,54 +14,236 @@ namespace Xylox.Audio
 {
     public class MusicService : IMusicService
     {
-        public Task<string> ConnectAsync(ulong voiceChannel, ulong textChannel)
+        private readonly LavaNode _lavaNode;
+        private readonly DiscordSocketClient _discordClient;
+
+        public MusicService(LavaNode lavaNode, DiscordSocketClient discordClient)
+        {
+            _lavaNode = lavaNode;
+            _discordClient = discordClient;
+            _lavaNode.OnTrackEnded += TrackEndedAsync;
+        }
+
+        public async Task InitializeWhenReadyAsync()
+        {
+            await _lavaNode.ConnectAsync();
+        }
+
+        public async Task<string> JoinAsync(ulong voiceId, ulong textId)
+        {
+            var voiceChannel = GetDiscordChannel<IVoiceChannel>(voiceId);
+            var textChannel = GetDiscordChannel<ITextChannel>(textId);
+
+            await _lavaNode.JoinAsync(voiceChannel, textChannel);
+
+            return $"Joined {voiceChannel.Name}, bound to {textChannel.Name}";
+        }
+
+        public async Task<string> LeaveAsync(ulong voiceId)
+        {
+            var voiceChannel = GetDiscordChannel<IVoiceChannel>(voiceId);
+            await _lavaNode.LeaveAsync(voiceChannel);
+
+            return $"Now Left {voiceChannel.Id}";
+        }
+
+        public Task <IEnumerable<IXyloxTrack>> GetPlaylistAsync(ulong id)
+        {
+            var guild = GetGuild(id);
+            var tracks = new List<IXyloxTrack>();
+            
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                foreach (var track in player.Queue.Items)
+                {
+                    var xyTrack = ConvertTrackToXyloxTrack(track, true);
+                    tracks.Add(xyTrack);
+                }
+            }
+
+            return Task.FromResult((IEnumerable<IXyloxTrack>)tracks);
+        }
+
+        public async Task<string> PauseAsync(ulong id)
+        {
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (player.PlayerState is PlayerState.Paused)
+                {
+                    return "Unable to pause, player is already paused. Did you mean to resume?";
+                }
+                else
+                {
+                    await player.PauseAsync();
+                    return "Player now paused";
+                }
+            }
+
+            return "Error when trying to pause, please contact my creator.";
+        }
+
+        public async Task<IXyloxTrack> PlayTrackAsync(string query, ulong id)
+        {
+            var search = await _lavaNode.SearchYouTubeAsync(query);
+            var track = search.Tracks.FirstOrDefault();
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (player.PlayerState is PlayerState.Playing)
+                {
+                    player.Queue.Enqueue(track);
+                    return ConvertTrackToXyloxTrack(track, true);
+                }
+                else
+                {
+                    await player.PlayAsync(track);
+                    return ConvertTrackToXyloxTrack(track, false);
+                }
+            }
+
+            return new BlankXyloxTrack();
+        }
+
+        public async Task<string> ResumeAsync(ulong id)
+        {
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (player.PlayerState is PlayerState.Paused)
+                {
+                    await player.ResumeAsync();
+                    return "Player has now resumed.";
+                }
+                else
+                {
+                    return "Unable to resume, player is already playing. Did you mean to pause?";
+                }
+            }
+
+            return "Error when trying to resume, please contact my creator.";
+        }
+
+        public async Task<string> SetVolumeAsync(int level, ulong id)
+        {
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (IsValidVolume(level))
+                {
+                    await player.UpdateVolumeAsync((ushort)level);
+                    return $"Player volume set to {level}%";
+                }
+                else
+                {
+                    return $"Something went wrong. Please ensure level is between 1 - 100.";
+                }
+            }
+
+            return "Error trying to set volume, please contact my creator.";
+        }
+
+        public Task<IXyloxTrack> SkipTrackAsync(int trackId, ulong id)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<XyloxTrack>> GetPlaylistAsync(ulong id)
+        public async Task<IXyloxTrack> SkipTrackAsync(ulong id)
         {
-            throw new NotImplementedException();
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (player.Queue.Count < 1)
+                {
+                    return new BlankXyloxTrack();
+                }
+                else
+                {
+                    await player.SkipAsync();
+                    var track = ConvertTrackToXyloxTrack(player.Track, false);
+                    return track;
+                }
+            }
+
+            return new BlankXyloxTrack();
         }
 
-        public Task<string> PauseAsync(ulong id)
+        public async Task<string> StopAsync(ulong id)
         {
-            throw new NotImplementedException();
+            var guild = GetGuild(id);
+
+            if (_lavaNode.TryGetPlayer(guild, out var player))
+            {
+                if (player.PlayerState is PlayerState.Playing)
+                {
+                    await player.StopAsync();
+                    return "Player has now stopped playing.";
+                }
+                else
+                {
+                    return "Player didn't seem to be playing anything.";
+                }
+            }
+            else
+            {
+                return "Error stopping player, please contact my creator.";
+            }
         }
 
-        public Task<XyloxTrack> PlayTrackAsync(string query, ulong id)
+        private async Task TrackEndedAsync(TrackEndedEventArgs trackEndArgs)
         {
-            throw new NotImplementedException();
+            if (!trackEndArgs.Reason.ShouldPlayNext())
+                return;
+
+            if (!trackEndArgs.Player.Queue.TryDequeue(out var track))
+                await trackEndArgs.Player.TextChannel.SendMessageAsync("No more tracks to play.");
+
+            await trackEndArgs.Player.PlayAsync(track);
+            await trackEndArgs.Player.TextChannel.SendMessageAsync($"Now playing: {track.Title}");
         }
 
-        public Task<XyloxTrack> PlayTrackAsync(Uri uri, ulong id)
+        private T GetDiscordChannel<T>(ulong id)
         {
-            throw new NotImplementedException();
+            var channel = _discordClient.GetChannel(id);
+
+            if (channel is T returnChannel)
+                return returnChannel;
+            else
+                throw new InvalidCastException($"Music Service: Could not cast channel to {typeof(T)}");
         }
 
-        public Task<string> ResumeAsync(ulong id)
+        private IGuild GetGuild(ulong id)
         {
-            throw new NotImplementedException();
+            var guild = _discordClient.GetGuild(id) as IGuild;
+
+            if (guild is null)
+                throw new InvalidCastException("Music Service: Unable to cast guild to IGuild");
+
+            return guild;
         }
 
-        public Task<string> SetVolumeAsync(int level, ulong id)
+        private bool IsValidVolume(int level)
         {
-            throw new NotImplementedException();
+            if (level < 1 || level > 100)
+                return false;
+            else
+                return true;
         }
 
-        public Task<XyloxTrack> SkipTrackAsync(int trackId, ulong id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<XyloxTrack> SkipTrackAsync(ulong id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> StopAsync(ulong id)
-        {
-            throw new NotImplementedException();
-        }
+        private XyloxTrack ConvertTrackToXyloxTrack(LavaTrack track, bool isQueued)
+            => new XyloxTrack
+            {
+                Title = track.Title,
+                Author = track.Author,
+                Duration = track.Duration,
+                Url = track.Url,
+                IsQueued = isQueued
+            };
     }
 }
