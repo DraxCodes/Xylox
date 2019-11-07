8cbe377cb65bb59f64ce9205841fb219758a7429
Joel Parkinson <joelparkinson@live.co.uk>
2019-11-07 00:15:50 +0000

    Refactor XyloxModuleBase

--- src/Xylox.Discord/Commands/XyloxModuleBase.cs
+++ src/Xylox.Discord/Commands/XyloxModuleBase.cs
@@ -7,8 +7,6 @@ namespace Xylox.Discord.Commands
     public class XyloxModuleBase : ModuleBase<XyloxCommandContext>
     {
         protected async Task ReplyEmbedAsync(EmbedBuilder embed)
-        {
-            await Context.Channel.SendMessageAsync(embed: embed.Build());
-        }
+            => await Context.Channel.SendMessageAsync(embed: embed.Build());
     }
 }
