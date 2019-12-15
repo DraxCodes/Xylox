d2c8040e72c5f7a70a2ad9f939ccaff5a244e5f5
Joel Parkinson <joelparkinson@live.co.uk>
2019-12-15 22:19:14 +0000

    Add Kick command
    Allows members with guild kick permissions to kick a user for a specified reason.

---
+++ src/Xylox.Discord/Commands/Modules/Moderation.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xylox.Discord.Commands.Modules
+{
+    public class Moderation : XyloxModuleBase
+    {
+        [Command("Kick")]
+        [RequireBotPermission(GuildPermission.KickMembers)]
+        [RequireUserPermission(GuildPermission.KickMembers)]
+        public async Task KickCommand(
+            [Name("User")] [Summary("The user you wish to kick.")] SocketGuildUser user,
+            [Name("User")] [Summary("The reason you are kicking the user.")]string reason)
+        {
+            await user.KickAsync(reason);
+            await ReplyAsync($"{user.Mention} has been kicked for: {reason}.");
+        }
+    }
+}
