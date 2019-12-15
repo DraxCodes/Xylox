6c22c2ecb1cc63a09812281536c1ae181e337882
Joel Parkinson <joelparkinson@live.co.uk>
2019-12-15 23:09:25 +0000

    Start implementation of purge

--- src/Xylox.Discord/Commands/Modules/Moderation.cs
+++ src/Xylox.Discord/Commands/Modules/Moderation.cs
@@ -1,6 +1,9 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Xylox.Discord.Commands.Modules
@@ -40,6 +43,30 @@ namespace Xylox.Discord.Commands.Modules
             await user.BanAsync(pruneDays: 7);
             await ReplyAsync($"{user.Mention} has been soft banned. 7 Days of messages pruned.");
             await Context.Guild.RemoveBanAsync(user);
+        }
+
+        [Command("Purge")]
+        [RequireBotPermission(GuildPermission.ManageMessages)]
+        [RequireUserPermission(GuildPermission.ManageMessages)]
+        public async Task Purge(
+            [Name("User")] [Summary("The user you wish to purge.")] SocketGuildUser user,
+            [Name("Amount")] [Summary("The amount of messages you wish to purge. (Default 20)")] int amount = 20)
+        {
+            if (Context.Channel is SocketTextChannel textChannel)
+            {
+                var messages = await FetchAndVerfiyMessages(textChannel, amount);
+            }
+            else
+            {
+                
+            }
+        }
+
+        private async Task<IEnumerable<IMessage>> FetchAndVerfiyMessages(SocketTextChannel channel, int amount)
+        {
+            var oldestAllowedTimestamp = DateTime.Now.AddDays(-14);
+            var channelMessages = await channel.GetMessagesAsync(amount).FlattenAsync();
+            return channelMessages.Where(m => m.CreatedAt > oldestAllowedTimestamp);
         }
     }
 }
